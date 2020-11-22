4b949d4c6bad9158025cee5516c6dab75f974c75
Manuel <m.a.n.u.e.l@posteo.net>
2020-11-22 23:08:45 +0100

    Update travis.yml

--- build/centos8/run.sh
+++ build/centos8/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker build -t minicloud_centos8 .
 docker run --network host -d -P --name cloud_on_centos8 minicloud_centos8
+
+sleep 10
+
+curl -I localhost/login 2> /dev/null | grep '501'
+curl -I localhost:8290 2> /dev/null | grep '401'
+
+sleep 10
+
+docker stop cloud_on_centos8
+docker rm cloud_on_centos8
--- build/focal/run.sh
+++ build/focal/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker build -t minicloud_focal .
 docker run --network host -d -P --name cloud_on_focal minicloud_focal
+
+sleep 10
+
+curl -I localhost/login 2> /dev/null | grep '501'
+curl -I localhost:8290 2> /dev/null | grep '401'
+
+sleep 10
+
+docker stop cloud_on_focal
+docker rm cloud_on_focal
